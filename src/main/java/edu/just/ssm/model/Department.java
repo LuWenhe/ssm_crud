7160c40ac4ade0d22644e1f81e605ef4112cf50f
luwenhe12345 <luwenhe12345@126.com>
2018-08-05 09:23:30 +0800

    第一次提交

---
+++ src/main/java/edu/just/ssm/model/Department.java
@@ -0,0 +1,23 @@
+package edu.just.ssm.model;
+
+public class Department {
+    private Integer deptId;
+
+    private String name;
+
+    public Integer getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Integer deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
---
+++ src/main/java/edu/just/ssm/model/Employee.java
@@ -0,0 +1,64 @@
+package edu.just.ssm.model;
+
+public class Employee {
+    private Integer empId;
+
+    private String name;
+
+    private String gender;
+
+    private String email;
+
+    private Integer dId;
+    
+    //希望查询员工信息的同时, 部门信息也是查询好的
+    private Department department;
+    
+    public void setDepartment(Department department) {
+		this.department = department;
+	}
+    
+    public Department getDepartment() {
+		return department;
+	}
+
+    public Integer getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Integer empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender == null ? null : gender.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Integer getdId() {
+        return dId;
+    }
+
+    public void setdId(Integer dId) {
+        this.dId = dId;
+    }
+}
